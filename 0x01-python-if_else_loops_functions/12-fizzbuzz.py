51290bc7c2d0022fc8bf6e9dba00f8617549134e
blessing-law <blessing.lawrence77@gmail.com>
2022-11-30 11:47:49 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101)
-        if i == % 3:
+    for i in range(101):
+        if i % 3 == 0:
             print("Fizz")
-        elif i == % 5:
+        elif i % 5 == 0:
             print("Buzz")
-        elif (i == % 3) and (i == %5)
+        elif (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz")
         else:
             print(i)
