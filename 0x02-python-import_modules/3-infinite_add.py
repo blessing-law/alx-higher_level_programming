c71d059d80c76e6987a7f746d91a2a08d49df0da
blessing-law <blessing.lawrence77@gmail.com>
2022-12-01 12:06:28 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,5 +2,7 @@
 if __name__ == "__main__ ":
     from sys import argv
 
+    add = 0
     for i in range(len(argv) - 1):
-        print("{}".format(int(argv[i + 1])))
+        add += int(argv[i + 1])
+    print("{}".format(add))
