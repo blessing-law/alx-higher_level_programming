23d47e46545cd123aaeed3e28d63ffda6be5e391
blessing-law <blessing.lawrence77@gmail.com>
2022-12-06 06:30:37 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence != '':
-        first_chr = sentence[0]
+        first = sentence[0]
     else:
-        sentence = None
-    return (len(sentence), first_chr)
+        first = None
+    return (len(sentence), first)
