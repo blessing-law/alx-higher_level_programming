a71081a88e79a16f41cd4706ab08cd3d4c663301
blessing-law <blessing.lawrence77@gmail.com>
2022-12-09 01:35:00 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newList = list(my_list)
+    j = 0
     for i in my_list:
         if i == search:
             newList[j] = replace
