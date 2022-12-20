8c896c1edb024061ef7974fdcf59812b383ebc05
blessing-law <blessing.lawrence77@gmail.com>
2022-12-20 10:15:25 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Class sqaure: create square class
+"""
+
+
+class Square:
+    """Define sqaure"""
+
+    def __init__(self, size=0):
+        """Initialise square class.
+        Args:
+            Size: size of the square.
+        Raises:
+            TypeError: if size is not int.
+            ValueError: if size is less than zero.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area of sqaure"""
+        return(self.__size * self.__size)
