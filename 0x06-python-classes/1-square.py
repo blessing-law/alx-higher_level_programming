1b0b81a03a3617adee01de88a70043293f7cc648
blessing-law <blessing.lawrence77@gmail.com>
2022-12-20 07:38:31 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square
+
+Define square class with private instance attribue size
+"""
+
+
+class Square:
+    """Defines Square"""
+
+    def __init__(self, size):
+        """initialise variables"""
+
+        self.__size = size
