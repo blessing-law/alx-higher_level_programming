42936903007bc80e73e1a54c77a0771bec9d9923
blessing-law <blessing.lawrence77@gmail.com>
2023-01-09 10:18:44 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Defines class base geometery
+"""
+
+
+class BaseGeometry:
+    """Class represents a base geometry"""
+
+    def area(self):
+        """ """
+        raise Exception("area() is not impemented")
