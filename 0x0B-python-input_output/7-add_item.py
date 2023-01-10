325235779d5a9c6d6bbd21780852ec9691d311a2
blessing-law <blessing.lawrence77@gmail.com>
2023-01-10 10:13:55 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module that adds all arguments to a python list and saves to file"""
-import sys
 
+
+import sys
 
 if __name__ = "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
