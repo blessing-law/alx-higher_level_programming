4478e1a257f4f0617b063465141bd01bcefbe92a
blessing-law <blessing.lawrence77@gmail.com>
2023-01-10 09:23:23 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a file appending function"""
 
+
 def append_write(filename="", text=""):
     """Appends string to the end of text file"""
-    with open(filename , "a", encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
