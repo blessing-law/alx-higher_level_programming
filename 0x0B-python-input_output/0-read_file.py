5eb6f56e8291fd29b77e99afe7300b953621d92a
blessing-law <blessing.lawrence77@gmail.com>
2023-01-10 07:30:30 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Module that reads and prints contentes of file
-to standout"""
+Module that defines a text file reading function"""
 
 
 def read_file(filename=""):
-    """Function that prints to stdout"""
+    """Function that prints contents of file to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
