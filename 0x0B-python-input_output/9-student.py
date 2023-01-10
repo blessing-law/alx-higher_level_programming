c2c398320b549066c6acd8ab96e7abce8d63ebd1
blessing-law <blessing.lawrence77@gmail.com>
2023-01-10 10:30:04 -0800

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class called student"""
+
+
+class Student:
+    """Reprents a student"""
+
+    def __inti__(self, first_name, last_name, age):
+        """Initioalize student
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of othe student
+            age(int): The age of the student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """get dictionary representaion of student"""
+        return self.__dict__
