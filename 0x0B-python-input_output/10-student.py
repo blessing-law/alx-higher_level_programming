156f23212960e5fcddb90240721b131a6e85443e
blessing-law <blessing.lawrence77@gmail.com>
2023-01-10 10:44:16 -0800

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -22,5 +22,5 @@ class Student:
         """get dictionary representaion of student"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(slef, k)}
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
